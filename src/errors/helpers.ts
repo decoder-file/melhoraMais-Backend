8a08240fdc3859e2c2c6894a0d4334bf99759689
Yan Soares <yts.1995@gmail.com>
2022-07-30 12:51:10 -0300

    chore: solving middleware message problem

--- src/errors/helpers.ts
+++ src/errors/helpers.ts
@@ -1,2 +1,11 @@
 export class RequestError extends Error {}
-export class NotAuhorizedError extends Error {}
+
+export class NotAuhorizedError {
+  public readonly message: string
+  public readonly statusCode: number
+
+  constructor (message: string, statusCode = 401) {
+    this.message = message
+    this.statusCode = statusCode
+  }
+}
--- src/middlewares/ensure-authenticated.ts
+++ src/middlewares/ensure-authenticated.ts
@@ -6,15 +6,15 @@ import { verify } from 'jsonwebtoken'
 
 type IPayload = { sub: string }
 
-export async function ensureAuthenticate (request: Request, _: Response, next: NextFunction) {
+export function ensureAuthenticate (request: Request, res: Response, next: NextFunction) {
   const authHeader = request.headers.authorization
-  if (!authHeader) throw new NotAuhorizedError('Token is missing.')
+  if (!authHeader) return res.status(401).json({ message: 'Token is missing.' })
   const [, token] = authHeader.split(' ')
   try {
     const { sub: user_id } = verify(token, env.jwt.secret) as IPayload
     request.user = { id: user_id }
     next()
-  } catch {
-    throw new NotAuhorizedError('Invalid token.')
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid token.' })
   }
 }
