1a86f230b0e474a023235748b1959e188af7813d
Yan Soares <yts.1995@gmail.com>
2022-07-31 22:09:46 -0300

    chore: add docs

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,8 +1,11 @@
+import { resolve } from 'node:path'
 import express, { Router, json } from 'express'
 import cors from 'cors'
+import YAML from 'yamljs'
+import { serve, setup } from 'swagger-ui-express'
 
 import { TagCalculationControllerFactory, UserControllerFactory, LoginControllerFactory, CalculationControllerFactory } from '@/main/factories/controllers'
-import { ensureAuthenticate } from '@/middlewares/ensure-authenticated'
+import { ensureAuthenticate } from '@/middlewares'
 
 export const app = express()
 const router = Router()
@@ -10,6 +13,7 @@ const calculationController = CalculationControllerFactory()
 const loginController = LoginControllerFactory()
 const tagCalculationController = TagCalculationControllerFactory()
 const userController = UserControllerFactory()
+const swaggerDocument = YAML.load(resolve(__dirname, '../docs/api-spec.yaml'))
 
 app.use(cors())
 app.use(json())
@@ -17,6 +21,8 @@ app.use(json())
 router.post('/users', async (req, res) => userController.create(req, res))
 router.post('/login', async (req, res) => loginController.handle(req, res))
 
+app.use('/docs', serve, setup(swaggerDocument))
+app.use(router)
 app.use(ensureAuthenticate)
 
 router.get('/users', async (req, res) => userController.get(req, res))
@@ -35,5 +41,3 @@ router.get('/tag-calculations/:id', async (req, res) => tagCalculationController
 router.get('/tag-calculations', async (req, res) => tagCalculationController.get(req, res))
 router.delete('/tag-calculations/:id', async (req, res) => tagCalculationController.delete(req, res))
 router.patch('/tag-calculations/:id', async (req, res) => tagCalculationController.update(req, res))
-
-app.use(router)
--- src/middlewares/ensure-authenticated.ts
+++ src/middlewares/ensure-authenticated.ts
@@ -5,13 +5,13 @@ import { verify } from 'jsonwebtoken'
 
 type IPayload = { sub: string }
 
-export function ensureAuthenticate (request: Request, res: Response, next: NextFunction) {
-  const authHeader = request.headers.authorization
+export function ensureAuthenticate (req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization
   if (!authHeader) return res.status(401).json({ message: 'Token is missing.' })
   const [, token] = authHeader.split(' ')
   try {
     const { sub: user_id } = verify(token, env.jwt.secret) as IPayload
-    request.user = { id: user_id }
+    req.user = { id: user_id }
     next()
   } catch (err) {
     return res.status(401).json({ message: 'Invalid token.' })
