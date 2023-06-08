a9a6a3f9813abfd6d5cb42d6db718e573434b6cd
Yan Soares <yts.1995@gmail.com>
2023-06-08 20:32:41 -0300

    chore: change from db-url to specify each property

--- src/repositories/pg-connection.ts
+++ src/repositories/pg-connection.ts
@@ -5,6 +5,10 @@ export const dbSource = new DataSource({
   type: 'postgres',
   migrations: [`./${dir}/repositories/migrations/*.{js,ts}`],
   entities: [`./${dir}/repositories/entities/*.{js,ts}`],
-  url: environment.pg.url,
+  host: environment.pg.host,
+  port: environment.pg.port,
+  username: environment.pg.user,
+  password: environment.pg.password,
+  database: environment.pg.database,
   synchronize: false
 })
